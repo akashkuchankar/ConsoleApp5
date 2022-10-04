0a37261a97a0abba5b516fa2cb60b5f22a63d3c9
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 03:15:27 -0700

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, M;
+                Console.WriteLine( "enter the number");
+           N =Convert.ToInt32(Console.ReadLine());
+            for(M=1; M<=10; M++)
+            {
+                Console.WriteLine("{0}*{1}={2}",N,M,(N*M));
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
